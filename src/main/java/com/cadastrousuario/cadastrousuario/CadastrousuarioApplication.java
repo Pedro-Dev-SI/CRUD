5613c5e8771ba200cfd0a704e4bf2764b9e8725c
Pedro Selvate <pedroselvate72@gmail.com>
2021-03-14 09:54:44 -0300

    Primeiro commit

---
+++ src/main/java/com/cadastrousuario/cadastrousuario/CadastrousuarioApplication.java
@@ -0,0 +1,13 @@
+package com.cadastrousuario.cadastrousuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrousuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrousuarioApplication.class, args);
+	}
+
+}
